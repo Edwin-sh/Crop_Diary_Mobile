41a956d247050c5d06091f4964222254ac52e347
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-07-21 12:32:52 -0500

    Configured typography

--- app/src/main/java/com/myapps/cropdiarymobile/ui/theme/Type.kt
+++ app/src/main/java/com/myapps/cropdiarymobile/ui/theme/Type.kt
@@ -2,33 +2,156 @@ package com.myapps.cropdiarymobile.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.myapps.cropdiarymobile.R
 
+val Roboto = FontFamily(
+    Font(R.font.roboto_black, FontWeight.Black),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+    Font(R.font.roboto_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_thin, FontWeight.Thin),
+)
+
+val Junegull = FontFamily(
+    Font(R.font.junegull, FontWeight.Bold),
+    Font(R.font.junegull, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.junegull, FontWeight.Light),
+    Font(R.font.junegull, FontWeight.Medium),
+    Font(R.font.junegull, FontWeight.Normal),
+)
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+val AppTypography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        lineHeight = 64.sp,
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 52.sp,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 44.sp,
+        fontSize = 36.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 40.sp,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 36.sp,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 32.sp,
+        fontSize = 24.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontStyle = FontStyle.Italic,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Italic,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Italic,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 11.sp
     )
-    */
+)
+val SecondAppTypography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = Junegull,
+        lineHeight = 28.sp,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Junegull,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Junegull,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Junegull,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Junegull,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Junegull,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    )
 )

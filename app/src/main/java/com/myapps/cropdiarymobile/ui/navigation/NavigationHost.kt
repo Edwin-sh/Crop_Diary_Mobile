70892bf3296e18f30c297ab668756282ac63e911
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-08-03 21:50:19 -0500

    Update NavigationHost.kt

--- app/src/main/java/com/myapps/cropdiarymobile/ui/navigation/NavigationHost.kt
+++ app/src/main/java/com/myapps/cropdiarymobile/ui/navigation/NavigationHost.kt
@@ -4,17 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.ExperimentalPagerApi
 import com.myapps.cropdiarymobile.core.WindowSize
 import com.myapps.cropdiarymobile.ui.navigation.Destinations.*
-import com.myapps.cropdiarymobile.ui.screens.*
+import com.myapps.cropdiarymobile.ui.screens.onBoarding.OnBoardingScreen
+import com.myapps.cropdiarymobile.ui.screens.splashScreen.SplashViewScreen
 
+@ExperimentalPagerApi
 @Composable
 fun AppNavigation(windowSize: WindowSize){
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = SplashScreen.route){
         composable(SplashScreen.route){
-            SplashViewScreen(windowSize, navController)
+            SplashViewScreen(navController)
         }
         composable(OnBoardingScreen.route){
             OnBoardingScreen()

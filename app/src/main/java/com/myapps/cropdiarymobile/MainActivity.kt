2de2958d0a0803a4eec4fb3d6ad13c308b045da7
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-08-13 15:46:15 -0500

    Updated MainActivity

--- app/src/main/java/com/myapps/cropdiarymobile/MainActivity.kt
+++ app/src/main/java/com/myapps/cropdiarymobile/MainActivity.kt
@@ -3,28 +3,23 @@ package com.myapps.cropdiarymobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.pager.ExperimentalPagerApi
-import com.myapps.cropdiarymobile.core.rememberWindowSize
 import com.myapps.cropdiarymobile.ui.navigation.AppNavigation
 import com.myapps.cropdiarymobile.ui.theme.CropDiaryAppTheme
 import com.myapps.cropdiarymobile.ui.theme.CropDiaryMobileTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalPagerApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CropDiaryAppTheme {
-                val window = rememberWindowSize()
-                AppNavigation(windowSize = window)
+                AppNavigation()
             }
         }
     }
@@ -35,7 +30,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     CropDiaryMobileTheme {
-        val window = rememberWindowSize()
-        AppNavigation(windowSize = window)
+        //val window = rememberWindowSize()
+        AppNavigation()
     }
 }

bed5701e7c8811a9791deda48e5de59f569bd88d
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-08-13 15:46:39 -0500

    Added preferences constants

---
+++ app/src/main/java/com/myapps/cropdiarymobile/core/util/PreferencesConstants.kt
@@ -0,0 +1,6 @@
+package com.myapps.cropdiarymobile.core.util
+
+object PreferencesConstants {
+    const val DATA_STORE_NAME = "CROP_DIARY_PREFERENCES"
+    const val ON_BOARDING_STATE = "ON_BOARDING_COMPLETE"
+}

901320478b7fd7558fd9837db863529e2aab89aa
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-08-13 15:40:00 -0500

    Updated class WindowInformation

--- app/src/main/java/com/myapps/cropdiarymobile/core/WindowInformation.kt
+++ app/src/main/java/com/myapps/cropdiarymobile/core/WindowInformation.kt
@@ -2,50 +2,112 @@ package com.myapps.cropdiarymobile.core
 
 import android.content.res.Configuration
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
-data class WindowSize(
-    val width: WindowType,
-    val height: WindowType
+data class WindowInformation(
+    val windowOrientation: WindowOrientation,
+    val windowGrid: WindowGrid
 )
-
-enum class WindowType { Compact, Medium, Expanded }
-
 enum class WindowOrientation { Portrait, Landscape }
+data class WindowGrid(
+    val minimumSpace: Dp,
+    val margin: Dp,
+    private val columnWidth: Dp,
+    private val rowHeight: Dp
+) {
+    fun width(columns: Double): Dp =
+        ((columnWidth.value * columns) + (minimumSpace.value * (columns - 1))).dp
+
+    fun height(rows: Double): Dp = ((rowHeight.value * rows) + (minimumSpace.value * (rows - 1))).dp
+    fun width(columns: Int): Dp = ((columnWidth * columns) + (minimumSpace * (columns - 1)))
+    fun height(rows: Int): Dp = ((rowHeight * rows) + (minimumSpace * (rows - 1)))
+}
 
 @Composable
-fun rememberWindowSize(): WindowSize {
-    val configuration = LocalConfiguration.current
-    val screenWidth by remember(key1 = configuration) {
-        mutableStateOf(configuration.screenWidthDp)
-    }
-    val screenHeight by remember(key1 = configuration) {
-        mutableStateOf(configuration.screenHeightDp)
-    }
+private fun getWindowOrientation(): WindowOrientation {
+    return if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) WindowOrientation.Landscape else WindowOrientation.Portrait
+}
 
-    return WindowSize(
-        width = getScreenWidth(screenWidth),
-        height = getScreenHeight(screenHeight)
+@Composable
+private fun getWindowGrid(): WindowGrid {
+    val orientation = getWindowOrientation()
+    val configuration = LocalConfiguration.current
+    val width = configuration.screenWidthDp
+    val height = configuration.screenHeightDp
+    val margin = getMargin(windowOrientation = orientation, width = width)
+    val minimumSpace = margin / 3
+    val availableWidth = getAvailableWidth(
+        windowOrientation = orientation,
+        width = width,
+        margin = margin,
+        minimumSpace = minimumSpace
+    )
+
+    val availableHeight = getAvailableHeight(
+        windowOrientation = orientation,
+        height = height,
+        margin = margin,
+        minimumSpace = minimumSpace
+    )
+    val columnWidth = getColumnWidth(
+        windowOrientation = orientation,
+        availableWidth = availableWidth
+    )
+    val rowHeight = getRowHeight(
+        windowOrientation = orientation,
+        availableHeight = availableHeight
+    )
+    return WindowGrid(
+        minimumSpace = minimumSpace.dp,
+        margin = margin.dp,
+        columnWidth = columnWidth.dp,
+        rowHeight = rowHeight.dp
     )
 }
 
 @Composable
-fun getWindowOrientation():WindowOrientation{
-    return if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) WindowOrientation.Landscape else WindowOrientation.Portrait
+fun getWindowInformation(): WindowInformation {
+    return WindowInformation(getWindowOrientation(), getWindowGrid())
 }
 
-fun getScreenWidth(width: Int): WindowType = when {
-    width < 600 -> WindowType.Compact
-    width < 840 -> WindowType.Medium
-    else -> WindowType.Expanded
+@Composable
+private fun getMargin(windowOrientation: WindowOrientation, width: Int) =
+    if (windowOrientation == WindowOrientation.Portrait) (width.div(20)) else (width.div(
+        30
+    ))
+
+@Composable
+private fun getAvailableWidth(
+    windowOrientation: WindowOrientation,
+    width: Int,
+    margin: Int,
+    minimumSpace: Int
+): Int {
+    val columns = if (windowOrientation == WindowOrientation.Portrait) 7 else 11
+    return width - ((margin * 2) + (minimumSpace * columns))
 }
 
-fun getScreenHeight(height: Int): WindowType = when {
-    height < 480 -> WindowType.Compact
-    height < 900 -> WindowType.Medium
-    else -> WindowType.Expanded
+@Composable
+private fun getAvailableHeight(
+    windowOrientation: WindowOrientation,
+    height: Int,
+    margin: Int,
+    minimumSpace: Int
+): Int {
+    val rows = if (windowOrientation == WindowOrientation.Portrait) 11 else 7
+    return height - ((margin * 2) + (minimumSpace * rows))
 }
+
+private fun getColumnWidth(
+    windowOrientation: WindowOrientation,
+    availableWidth: Int
+) = if (windowOrientation == WindowOrientation.Portrait) availableWidth / 8 else availableWidth / 12
+
+private fun getRowHeight(
+    windowOrientation: WindowOrientation,
+    availableHeight: Int
+) =
+    if (windowOrientation == WindowOrientation.Portrait) availableHeight / 12 else availableHeight / 8
 

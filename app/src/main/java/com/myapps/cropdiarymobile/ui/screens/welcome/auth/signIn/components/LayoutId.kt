8a200460a3a5ab1da27b41f3917e3603949acf96
Edwin Martínez <edwinmartinez11032003@gmail.com>
2023-09-04 06:19:02 -0500

    Login screen ui

---
+++ app/src/main/java/com/myapps/cropdiarymobile/ui/screens/welcome/auth/signIn/components/LayoutId.kt
@@ -0,0 +1,13 @@
+package com.myapps.cropdiarymobile.ui.screens.welcome.auth.signIn.components
+
+internal object LayoutId {
+    const val title = "title"
+    const val social_media_buttons = "social_media_buttons"
+    const val separator = "separator"
+    const val email_input = "email_input"
+    const val password_input = "password_input"
+    const val check_box = "check_box"
+    const val login_button = "login_button"
+    const val reset_password_text = "reset_password_text"
+    const val sign_up_text = "sign_up_text"
+}

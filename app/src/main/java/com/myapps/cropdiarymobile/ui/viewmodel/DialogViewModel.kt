414e489d65dff4bcbdb970fe5933bdfba522569c
Edwin Martinez <edwinmartinez11032003@gmail.com>
2024-02-12 15:22:18 -0500

    Project backup

--- app/src/main/java/com/myapps/cropdiarymobile/ui/viewmodel/DialogViewModel.kt
+++ app/src/main/java/com/myapps/cropdiarymobile/ui/viewmodel/DialogViewModel.kt
@@ -5,11 +5,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.myapps.cropdiarymobile.R
 import com.myapps.cropdiarymobile.data.state.DialogStateData
 import com.myapps.cropdiarymobile.di.coroutines.MainDispatcher
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,39 +24,45 @@ class DialogViewModel @Inject constructor(
     var state: DialogStateData by mutableStateOf(dialogState)
         private set
 
-    init {
-        clearDialogData()
-    }
+    val error = context.getString(R.string.error)
+    val info = context.getString(R.string.info)
+    val success = context.getString(R.string.success)
 
     private fun setData(
         title: String = "",
         message: String = "",
         positiveButtonText: String = "",
-        positiveButtonAction: () -> Unit = {}
+        positiveButtonAction: () -> Unit = {},
+        negativeButtonText: String = "",
+        negativeButtonAction: () -> Unit = {}
     ) {
         title.takeIf { it.isNotEmpty() }?.let { setTitle(it) }
-        message.takeIf { it.isNotEmpty() }?.let { setTitle(it) }
+        message.takeIf { it.isNotEmpty() }?.let { setMessage(it) }
         setPositiveButton(positiveButtonText, positiveButtonAction)
+        setNegativeButton(negativeButtonText, negativeButtonAction)
     }
 
-    fun showErrorDialog(message: String) {
-        setData(
-            title = context.getString(R.string.error),
-            message = message
-        )
-        state = state.copy(isShowing = true)
+    fun showDialog(
+        title: String = success,
+        message: String,
+        positiveButtonText: String = context.getString(R.string.accept),
+        positiveButtonAction: () -> Unit = {},
+        negativeButtonText: String = "",
+        negativeButtonAction: () -> Unit = {}
+    ) {
+        viewModelScope.launch(dispatcher) {
+            setData(
+                title,
+                message,
+                positiveButtonText,
+                positiveButtonAction,
+                negativeButtonText,
+                negativeButtonAction
+            )
+            state = state.copy(isShowing = true)
+        }
     }
 
-    fun showSuccessDialog() {
-        setTitle(context.getString(R.string.success))
-        state = state.copy(isShowing = true)
-    }
-
-
-
-    fun showDialog() {
-        state = state.copy(isShowing = true)
-    }
 
     fun hideDialog() {
         state = state.copy(isShowing = false)
@@ -69,28 +77,32 @@ class DialogViewModel @Inject constructor(
         state = state.copy(message = message)
     }
 
-    private fun clearDialogData() {
-        state = state.copy { dialogState }
-    }
-
-
     private fun setPositiveButton(
         text: String = "",
         onClick: () -> Unit = {}
     ) {
-        if (text.isNotEmpty()) {
-            state = state.copy(
-                positiveButtonText = text
-            )
-        }
-        if (onClick != {}) {
-            state = state.copy(
-                positiveButtonAction = onClick
-            )
-        }
         state = state.copy(
             positiveButtonText = text,
             positiveButtonAction = onClick
+        )
+    }
+
+    private fun setNegativeButton(
+        text: String = "",
+        onClick: () -> Unit = {}
+    ) {
+        state = state.copy(
+            negativeButtonText = text,
+            negativeButtonAction = onClick
+        )
+    }
+
+    fun clearDialogData() {
+        state = state.copy(
+            title = dialogState.title,
+            message = dialogState.message,
+            positiveButtonText = dialogState.positiveButtonText,
+            positiveButtonAction = dialogState.positiveButtonAction
         )
     }
 }
--- app/src/test/java/com/myapps/cropdiarymobile/ui/viewmodel/DialogViewModelTest.kt
+++ app/src/test/java/com/myapps/cropdiarymobile/ui/viewmodel/DialogViewModelTest.kt
@@ -1,11 +1,18 @@
 package com.myapps.cropdiarymobile.ui.viewmodel
 
 import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.myapps.cropdiarymobile.CoroutineTestRule
 import com.myapps.cropdiarymobile.data.state.DialogStateData
+import com.myapps.cropdiarymobile.di.coroutines.MainDispatcher
 import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class DialogViewModelTest {
@@ -16,14 +23,27 @@ class DialogViewModelTest {
     private lateinit var dialogStateData: DialogStateData
 
     @RelaxedMockK
+    @MainDispatcher
     private lateinit var dispatcher: TestCoroutineDispatcher
 
+    @RelaxedMockK
     private lateinit var dialogViewModel: DialogViewModel
+
+    @get:Rule
+    var rule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val ruleCoroutine = CoroutineTestRule()
 
     @Before
     fun onBefore() {
         MockKAnnotations.init(this)
         dialogViewModel = DialogViewModel(context, dialogStateData, dispatcher)
+    }
+
+    @After
+    fun onAfter() {
+        dialogViewModel.hideDialog()
     }
 
     @Test
@@ -32,6 +52,60 @@ class DialogViewModelTest {
         assert(dialogViewModel.state.title.isEmpty())
         assert(dialogViewModel.state.message.isEmpty())
         assert(dialogViewModel.state.positiveButtonText.isEmpty())
-        //assert(function=={})
+        assert(dialogViewModel.state.positiveButtonAction.invoke() == Unit)
     }
+
+    @Test
+    fun `when the clear dialog fun is called then the dialog state is set default values`() {
+        showGenericDialog()
+        assert(!dialogViewModel.state.isShowing)
+        assert(dialogViewModel.state.title.isEmpty())
+        assert(dialogViewModel.state.message.isEmpty())
+        assert(dialogViewModel.state.positiveButtonText.isEmpty())
+        assert(dialogViewModel.state.positiveButtonAction.invoke() == Unit)
+    }
+
+/*@Test
+/*fu`when the show dialog fun is called then the showing state is true`() = runTest{
+    dialogViewModel.showDialog(
+        title = "Test title",
+        message = "Test message",
+        positiveButtonText = "Test positive button",
+        positiveButtonAction = { println("Test") },
+        negativeButtonText = "Test negative button",
+        negativeButtonAction = { println("Test") }
+    )
+    dispatcher.scheduler.advanceUntilIdle()
+    assert(dialogViewModel.state.isShowing)
+}
+
+@Test
+fun `when the hide dialog fun is called then the showing state is false and the clear dialog fun is called`() {
+    showGenericDialog()
+    dialogViewModel.hideDialog()
+    assert(!dialogViewModel.state.isShowing)
+    verify { dialogViewModel.clearDialogData() }
+}
+
+@Test
+fun `when the show dialog fun is called with custom values then the dialog state is set`() {
+    showGenericDialog()
+    assert(dialogViewModel.state.title == "Test title")
+    assert(dialogViewModel.state.message == "Test message")
+    assert(dialogViewModel.state.positiveButtonText == "Test positive button")
+    assert(dialogViewModel.state.positiveButtonAction.invoke() == println("Test"))
+    assert(dialogViewModel.state.negativeButtonText == "Test negative button")
+    assert(dialogViewModel.state.negativeButtonAction.invoke() == println("Test"))
+}
+
+private fun showGenericDialog(){
+    dialogViewModel.showDialog(
+        title = "Test title",
+        message = "Test message",
+        positiveButtonText = "Test positive button",
+        positiveButtonAction = { println("Test") },
+        negativeButtonText = "Test negative button",
+        negativeButtonAction = { println("Test") }
+    )
+}*/
 }
